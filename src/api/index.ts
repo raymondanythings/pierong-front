39ec44cc49e76acdbce748ae37422bdad6b1df1d
raymond <raymondanything@gmail.com>
2022-12-21 14:37:57 +0900

    api 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import api from 'axios'
 
 const axios = api.create({ baseURL: process.env.REACT_APP_API_PATH })
-
+api.defaults.headers['Access-Control-Allow-Origin'] = 'https://api.pierong.site'
 export default axios
