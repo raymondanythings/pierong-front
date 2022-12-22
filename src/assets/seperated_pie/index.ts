6556babb3f1aa16e6dd60657cceb3d6a22755882
raymond <raymondanything@gmail.com>
2022-12-23 01:25:24 +0900

    TODO: 케이크 포지션

---
+++ src/assets/seperated_pie/index.ts
@@ -0,0 +1,25 @@
+import Pie_1 from './1.png'
+import Pie_2 from './2.png'
+import Pie_3 from './3.png'
+import Pie_4 from './4.png'
+import Pie_5 from './5.png'
+import Pie_6 from './6.png'
+import Pie_7 from './7.png'
+import Pie_8 from './8.png'
+import Dish from '../pie_s/plate.png'
+import CROWN from '../crown.png'
+
+const PIES = {
+	Pie_1,
+	Pie_2,
+	Pie_3,
+	Pie_4,
+	Pie_5,
+	Pie_6,
+	Pie_7,
+	Pie_8,
+	Dish,
+	CROWN
+}
+
+export default PIES
--- src/routes/Screen/Main.tsx
+++ src/routes/Screen/Main.tsx
@@ -1,7 +1,7 @@
 import Modal from 'components/Modal'
 import withNavigation from 'layout/withNavigation'
 import store from 'store'
-import PIES from 'assets/pie'
+import PIES from 'assets/seperated_pie'
 import MAIN from 'assets/main.png'
 import { motion, useAnimationControls, Variants } from 'framer-motion'
 const pieceVariants: Variants = {
@@ -22,38 +22,18 @@ const Main = () => {
 	console.log(atk)
 	return (
 		<div className="h-full relative bg-[url('assets/main.png')] bg-cover bg-no-repeat bg-center ">
-			{/* <img className="absolute" src={MAIN} /> */}
 			<motion.img className="absolute top-[] -left-[] w-3/5" src={PIES.CROWN} />
-			{/* <div className="relative top-[27.5vh] -right-[21.3vw]"> */}
-			<div onClick={() => console.log('dish')} className="max-w-[520px] absolute translate-x-[21%] translate-y-[63%]">
-				<motion.img onClick={() => console.log('img1')} className="" src={PIES.Dish} />
-				<div onClick={() => console.log('img2')}>
-					<motion.img className="absolute top-0 left-0" src={PIES.Pie_1} />
-				</div>
-				<div onClick={() => console.log('img3')}>
-					<motion.img className="absolute top-0 left-0" src={PIES.Pie_2} />
-				</div>
-				<div onClick={() => console.log('img4')}>
-					<motion.img className="absolute top-0 left-0" src={PIES.Pie_3} />
-				</div>
-				<div onClick={() => console.log('img5')}>
-					<motion.img className="absolute top-0 left-0" src={PIES.Pie_4} />
-				</div>
-				<div onClick={() => console.log('img6')}>
-					<motion.img className="absolute top-0 left-0" src={PIES.Pie_5} />
-				</div>
-				<div onClick={() => console.log('img7')}>
-					<motion.img className="absolute top-0 left-0" src={PIES.Pie_6} />
-				</div>
-				<div onClick={() => console.log('img8')}>
-					<motion.img className="absolute top-0 left-0" src={PIES.Pie_7} />
-				</div>
-				<div onClick={() => console.log('img9')}>
-					<motion.img className="absolute top-0 left-0" src={PIES.Pie_8} />
-				</div>
+			<div className="relative translate-x-[21%] translate-y-[63%]">
+				<motion.img className="" src={PIES.Dish} />
+				<motion.img className=" absolute object-contain top-0 left-[40vw] " src={PIES.Pie_1} />
+				<motion.img className=" absolute object-contain  top-[1vh] left-[17.4vw]" src={PIES.Pie_2} />
+				<motion.img className=" absolute object-contain top-[9.4vh] -left-[1vw]" src={PIES.Pie_3} />
+				<motion.img className=" absolute object-contain  top-[6.5vh] left-[41vw]" src={PIES.Pie_4} />
+				<motion.img className=" absolute object-contain  top-[33vh] left-[41vw]" src={PIES.Pie_5} />
+				<motion.img className=" absolute object-contain  top-[37.5vh] left-[40.5vw]" src={PIES.Pie_6} />
+				<motion.img className=" absolute object-contain  top-[37.5vh] -left-[0.7vw]" src={PIES.Pie_7} />
+				<motion.img className=" absolute object-contain  top-[37vh] left-[16vw]" src={PIES.Pie_8} />
 			</div>
-			{/* </div> */}
-			{/* <Modal>????</Modal> */}
 		</div>
 	)
 }
