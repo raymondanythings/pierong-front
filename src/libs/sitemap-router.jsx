736c89e47d4b519e48769146b17ac845690e508f
Raymond <73725736+raymondanythings@users.noreply.github.com>
2023-01-10 11:35:41 +0900

    sitemap generator

--- src/libs/sitemap-router.jsx
+++ src/libs/sitemap-router.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Route } from 'react-router'
 
-export default function () {
-	return (
-		<Route>
-			<Route path="/" />
-			<Route path="oauth" />
-			<Route path="room">
-				<Route path="feve" />
-				<Route path=":userId" />
-			</Route>
-			<Route path="privacy" />
-			<Route path="*" />
+export default (
+	<Route path="/">
+		<Route path="oauth" />
+		<Route path="room">
+			<Route path="feve" />
+			<Route path=":userId" />
 		</Route>
-	)
-}
+		<Route path="privacy" />
+		<Route path="*" />
+	</Route>
+)
